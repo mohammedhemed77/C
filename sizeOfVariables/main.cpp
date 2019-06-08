7fc234c1b1914ec90edb11376fd9dc28c4517491
Eng Sherif Mostafa <31868972+EngSherifMostafa@users.noreply.github.com>
2019-06-08 20:40:59 +0200

    Add files via upload

---
+++ sizeOfVariables/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<"bool size = "<<sizeof(bool)<<endl;
+
+    cout <<"short size = "<<sizeof(short)<<endl;
+
+    cout <<"int size = "<<sizeof(int)<<endl;
+
+    cout <<"float size = " << sizeof(float)<<endl;
+
+    cout <<"long size = "<< sizeof(long)<<endl;
+
+    cout <<"double size = " << sizeof(double)<<endl;
+
+    return 0;
+}
