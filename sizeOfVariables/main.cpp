ec23a48d096e627518cd1abe3cb4583e28981825
Eng Sherif Mostafa <31868972+EngSherifMostafa@users.noreply.github.com>
2019-06-09 12:34:11 +0200

    Update main.cpp

--- sizeOfVariables/main.cpp
+++ sizeOfVariables/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include "iomanip" //for setw (n) function
 using namespace std;
+
 int main()
 {
-    cout <<"bool size = "<<sizeof(bool)<<endl;
-
-    cout <<"short size = "<<sizeof(short)<<endl;
-
-    cout <<"int size = "<<sizeof(int)<<endl;
-
-    cout <<"float size = " << sizeof(float)<<endl;
-
-    cout <<"long size = "<< sizeof(long)<<endl;
-
-    cout <<"double size = " << sizeof(double)<<endl;
-
+    cout
+    << setw (22) << "long double size is : " << sizeof(long double) << endl << endl
+    << setw (22) << "double size is : " << sizeof(double) << endl
+    << setw (22) << "long long size is : " << sizeof(long long) << endl << endl
+    << setw (22) << "float size is : " << sizeof(float) << endl
+    << setw (22) << "long size is : " << sizeof(long) << endl
+    << setw (22) << "int size is : " << sizeof(int) << endl << endl
+    << setw (22) << "short int size is : " << sizeof(short int) << endl
+    << setw (22) << "short size is : " << sizeof(short) << endl << endl
+    << setw (22) << "char size is : " << sizeof(char) << endl
+    << setw (22) << "bool size is : " << sizeof(bool) << endl;
     return 0;
 }
