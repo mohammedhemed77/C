54f43b7bdc2eb21d7c47d2cc41a8f2c8fbcdb3d7
Eng Sherif Mostafa <31868972+EngSherifMostafa@users.noreply.github.com>
2019-06-25 14:34:56 +0200

    Update multiplyByRepeatedAddition.cpp

--- recursion/multiplyByRepeatedAddition.cpp
+++ recursion/multiplyByRepeatedAddition.cpp
@@ -1,23 +1,27 @@
 //multiply by repeated addition
 #include <iostream>
 using namespace std;
-int multiply (int ,int );
+int multiply(int ,int );
 
 int main()
 {
-    int m=5 , n=3;
-    cout << "result = " << multiply(m,n);
+    int x, y;
+    cout << "Enter two numbers: "; cin >> x >> y;
+    cout << "result = " << multiply(x,y);
     return 0;
 }
 
-int multiply (int m, int n)
+int multiply(int x, int y)
 {
-    if (n == 0)
+    if (x == 0 || y==0)
         return 0;
 
-    else if (n == 1)
-        return m;
+    else if (x==1)
+        return y;
+
+    else if (y==1)
+        return x;
 
     else
-        return m + multiply (m,n-1);
+        return x + multiply(x,y-1);
 }
