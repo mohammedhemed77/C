5cf9cc37cd87283fdf440ddff4e8645d38372890
Eng Sherif Mostafa <31868972+EngSherifMostafa@users.noreply.github.com>
2019-06-25 14:31:29 +0200

    Update fibonacciSeries.cpp

--- recursion/fibonacciSeries.cpp
+++ recursion/fibonacciSeries.cpp
@@ -1,31 +1,29 @@
 //Fibonacci series
 #include <iostream>
 using namespace std;
-int rFibNum (int a, int b, int n);
+int calcFib(int, int, int );
 
 int main()
 {
-    int firstFibNum;
-    int secondFibNum;
-    int nth;
-    cout << "Enter the first Fibonacci number: ";
-    cin >> firstFibNum;
-    cout << "\nEnter the second Fibonacci number: ";
-    cin >> secondFibNum;
-    cout << "\nEnter the position of the desired Fibonacci number: ";
-    cin >> nth;
-    cout<<"\nThe Fibonacci number at position "<<nth<<" is: "<<rFibNum(firstFibNum, secondFibNum, nth);
+    int num1;
+    int num2;
+    int pos;
+    cout << "Enter the first Fibonacci number: "; cin >> num1;
+    cout << "\nEnter the second Fibonacci number: "; cin >> num2;
+    cout << "\nEnter the position of the desired Fibonacci number: "; cin >> pos;
+
+    cout<<"\nThe Fibonacci number at position "<< pos <<" is: "<< calcFib(num1, num2, pos) << endl;
     return 0;
 }
 
-int rFibNum (int a, int b, int n)
+int calcFib(int num1, int num2, int pos)
 {
-    if (n == 1)
-        return a;
+    if (pos == 1)
+        return num1;
 
-    else if (n == 2)
-        return b;
+    else if (pos == 2)
+        return num2;
 
     else
-        return rFibNum(a, b, n - 1) + rFibNum(a, b, n - 2);
+        return calcFib(num1, num2, pos-1) + calcFib(num1, num2, pos-2);
 }
