e213fcd66291d470dbab0c95006deb402e4491cb
Eng Sherif Mostafa <31868972+EngSherifMostafa@users.noreply.github.com>
2019-06-25 14:24:31 +0200

    Update factorial.cpp

--- recursion/factorial.cpp
+++ recursion/factorial.cpp
@@ -5,14 +5,15 @@ int fact (int );
 
 int main ()
 {
-    int x=5;
-    cout << "factorial of "<< x << " = " << fact(x);
+    int x;
+    cout << "Enter number that you want to get factorial: "; cin >> x;
+    cout << "Factorial of "<< x << " = " << fact(x);
     return 0;
 }
 
 int fact (int x)
 {
-    if (x==0)
+    if (x==0 || x==1)
         return 1;
     else
         return x*fact(x-1);
